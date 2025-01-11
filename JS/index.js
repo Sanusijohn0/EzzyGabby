bd293743dfd811f8e69f22ff569d9e83a5526dde
sanusi john <sanusijohn0@gmail.com>
2025-01-11 21:35:02 +0100

    created load out

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".navbar");
+const loadout = document.getElementById("loadout");
+const loadoutBtn = document.getElementById("loadout-btn");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("none");
+});
+
+loadoutBtn.addEventListener("click", () => {
+  loadout.classList.toggle("hidden");
 });
